5144a52d8acd07cf44eacfc9bc6f0a6e489b806a
chenxingyu <wizard_cxy@hotmail.com>
2017-02-19 20:43:36 +0800

    finish fill screen

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,50 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+@8192
+D=A
+@totalwords
+M=D
+@SCREEN
+D=A
+
+@R0
+M=D
+
+(LOOP)
+@KBD
+D=M
+
+// if KBD !=0 set the whole screen black
+@BLACK
+D;JNE
+
+// store the white color in r1
+R1=0
+@UPDATE
+0;JMP
+
+(BLACK)
+// store black color in r1
+R1=-1
+// then update
+@UPDATE
+0;JMP
+
+(UPDATE)
+
+// if the old status is same with the new status continue
+@KBD
+D=M
+
+(FILL)
+// FILL program is used to fill color R1 from starting address R0
+
+@R1
+D=M
+
+@R0
+
+
+
+
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -1,42 +1,111 @@
 // This file is part of www.nand2tetris.org
 // and the book "The Elements of Computing Systems"
 // by Nisan and Schocken, MIT Press.
-// File name: projects/04/Mult.asm
+// File name: projects/04/Fill.asm
 
-// Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+// Runs an infinite loop that listens to the keyboard input. 
+// When a key is pressed (any key), the program blackens the screen,
+// i.e. writes "black" in every pixel. When no key is pressed, the
+// program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+// first put the starting address of the screen in R0
 
-// zero R2
-@R2
-M=0
+@8192
+D=A
+@SCREEN
+D=D+A
 
-// set i=0
-@i
-M=0
+// set the total words to 32x256+SCREEN
+@totalwords
+M=D
 
-(LOOP)
-// if i==R1 finish the loop goto end
-@i
+(START)
+// store the screen start address in R0
+@SCREEN
+D=A
+@R0
+M=D
+
+// get the keyboard input
+@KBD
 D=M
-@R1
-D=D-M
-@END
+// if D!=0 jump to black screen part
+@BLACK
+D;JGT
+
+// else jump to white screen part
+@WHITE
 D;JEQ
 
-@R0
-D=M
-@R2
-M=M+D
+(BLACK)
+// store the black color in R1
+@R1
+M=-1 // fill the screen with all 1 (1111111111111111)
 
-// i++
-@i
-M=M+1
-
-@LOOP
+@UPDATE
 0;JMP
 
-(END)
-@END
+(WHITE)
+// store the white color in R1
+@R1
+M=0
+@UPDATE
+0;JMP
+
+(UPDATE)
+//fill the whole screen with R1 color
+@R1
+D=M
+
+// first get the address which want to color
+@R0
+A=M
+//color it
+M=D
+
+// move to the next chunk of 16-bit pixel
+@R0
+D=M+1
+
+//totalwords - current addr 
+@totalwords
+D=M-D
+
+//already fill the whole screen no need to forward current write addr
+@START
+D;JLT
+
+@R0
+M=M+1
+
+@KBD
+D=M
+//if D>0 still need to fill
+@PRESSKEY
+D;JGT
+
+@NOTPRESSKEY
+0;JMP
+
+// press key situation
+(PRESSKEY)
+@R1
+D=M
+
+@START
+D;JEQ
+
+@UPDATE
+0;JMP
+
+//not press key situation
+(NOTPRESSKEY)
+@R1
+D=M
+
+@UPDATE
+D;JEQ
+
+@START
 0;JMP
